0651da629916037ee8cf4fcf6be078dd2d2fcf61
Hazim Bakar <hazimbaa@gmail.com>
2025-07-17 01:57:31 +0800

    create new course

--- src/components/pageWrapper.tsx
+++ src/components/pageWrapper.tsx
@@ -5,21 +5,26 @@ import { usePathname } from "next/navigation";
 
 const PageWrapper = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
+  const isCreatePage = pathname.includes("create");
 
   return (
     <div className="relative flex flex-col items-center justify-center w-full h-screen bg-gray-200">
       <Link
-        href="/"
+        href={isCreatePage ? pathname.replace("/create", "") : "/"}
         className="absolute top-4 left-4 cursor-pointer text-blue-600 hover:underline"
       >
         <ArrowLeftOutlined />
       </Link>
-      <Link
-        href={`${pathname}/create`}
-        className="absolute top-4 right-4 text-green-600 hover:text-green-800 font-bold text-xl"
-      >
-        <PlusOutlined />
-      </Link>
+      {isCreatePage ? (
+        <></>
+      ) : (
+        <Link
+          href={`${pathname}/create`}
+          className="absolute top-4 right-4 text-green-600 hover:text-green-800 font-bold text-xl"
+        >
+          <PlusOutlined />
+        </Link>
+      )}
 
       <div className="flex flex-col w-3/4 items-center">{children}</div>
     </div>

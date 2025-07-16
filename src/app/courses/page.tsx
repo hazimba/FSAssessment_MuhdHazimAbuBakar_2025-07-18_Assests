21d07dc64699f316308b7904de20857318a0a807
Hazim Bakar <hazimbaa@gmail.com>
2025-07-16 22:34:29 +0800

    add page

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,9 @@
+const Courses = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <h1 className="text-2xl font-bold">Courses</h1>
+      <p className="mt-4">This is the courses page.</p>
+    </div>
+  );
+};
+export default Courses;
---
+++ src/app/users/page.tsx
@@ -0,0 +1,9 @@
+const Users = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <h1 className="text-2xl font-bold">Users</h1>
+      <p className="mt-4">This is the users page.</p>
+    </div>
+  );
+};
+export default Users;

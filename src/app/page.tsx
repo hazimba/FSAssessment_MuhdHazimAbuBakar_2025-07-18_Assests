e97070bf0c5451e3103beef330908f6b00e92d94
Hazim Bakar <hazimbaa@gmail.com>
2025-07-16 23:36:33 +0800

    back button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <div className="flex flex-col items-center justify-center w-64 bg-gray-100 min-h-screen">
-        <h2 className="text-xl font-bold">Sidebar</h2>
+      <div className="flex flex-col items-center justify-center w-screen bg-gray-100 min-h-screen">
+        <h2 className="text-xl font-bold">Menu</h2>
         <Link href="/courses" className="text-blue-500">
           Courses
         </Link>
--- src/components/pageWrapper.tsx
+++ src/components/pageWrapper.tsx
@@ -1,7 +1,17 @@
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import Link from "next/link";
+
 const PageWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-200">
-      {children}
+    <div className="relative flex flex-col items-center justify-center w-full h-screen bg-gray-200">
+      <Link
+        href="/"
+        className="absolute top-4 left-4 cursor-pointer text-blue-600 hover:underline"
+      >
+        <ArrowLeftOutlined />
+      </Link>
+
+      <div className="flex flex-col items-center">{children}</div>
     </div>
   );
 };
